0ee2fe85db8758f0f4c66ad0dd5073a189da448b
Bhavya Jain <jainbhavya375@gmail.com>
2023-10-27 16:21:40 -0400

    Implemented the required feature

--- server.js
+++ server.js
@@ -51,7 +51,12 @@ const validateId = (id) => {
  * @param {any} priority
  */
 const validatePriority = (priority) => {
-  if (Number.isNaN(priority)) {
+  if (priority && !Number.isNaN(priority) && priority > 0) {
+    return {
+      valid: true,
+    }
+  
+  }else{
     return {
       valid: false,
       messageObj: {
@@ -60,8 +65,21 @@ const validatePriority = (priority) => {
       },
     };
   }
-  return {
-    valid: true,
+}
+
+const validateStatus = (status) =>{
+  if(status && ['complete','in-progress','backlog'].indexOf(status) > -1){
+    return {
+      valid:true,
+    }
+  }
+
+  return  {
+    valid: false,
+    messageObj: {
+    'message': 'Invalid status provided.',
+    'long_message': 'Priority can only be complete, in-progress or backlog.',
+     }, 
   }
 }
 
@@ -128,9 +146,63 @@ app.put('/api/v1/clients/:id', (req, res) => {
   /* ---------- Update code below ----------*/
 
 
+  try{
+    const priorityValid = validatePriority(priority).valid;
+    const statusValid = validateStatus(status).valid;
 
-  return res.status(200).send(clients);
+    if (client){
+
+      const maxPriorityAllowed = findMaxPriorityByStatus(clients,statusValid ? status : client.status);
+
+      if(statusValid){
+        updateStatus(client,status,maxPriorityAllowed);
+      }
+
+      if(priorityValid){
+        updatePriority(client,priority <= maxPriorityAllowed ? priority:maxPriorityAllowed);
+      }
+
+    const updatedClients = db.prepare('select * from clients').all();
+    
+    return res.status(200).send(updatedClients);
+    }
+
+    }catch(e){
+      console.log(e)
+      return res.status(500).send("Server Error: Could not process the given request");
+  }
 });
+
+const updatePriority=(client,newPriority)=>{
+  if(newPriority === client.priority){
+    return ;
+  }
+
+  if(newPriority < client.priority){
+    db.prepare('UPDATE clients SET priority = priority + 1 WHERE status= ? AND priority >= ? AND priority < ?').run(client.status,newPriority,client.priority);
+
+  }else if(newPriority > client.priority){
+    db.prepare('UPDATE clients SET priority = priority + 1 WHERE status= ? AND priority <= ? AND priority > ?').run(client.status,newPriority,client.priority);
+  }
+
+  db.prepare('UPDATE clients SET priority = ? WHERE id = ?').run(newPriority,client.id);
+
+}
+
+const updateStatus = (client,newStatus,maxPriorityNewStatus)=>{
+
+  if (newStatus === client.status){
+    return;
+  }
+  db.prepare('UPDATE clients SET priority = priority - 1 where priority > ? and status = ?').run(client.priority,client.status); //remove from old status
+  db.prepare('UPDATE clients SET priority = ?, status = ? where id = ?').run(maxPriorityNewStatus,newStatus,client.id); // add to bottom of the new status
+}
+
+const findMaxPriorityByStatus = (clients,status)=>{
+
+  if (clients.length > 0) return clients.filter(client=>client.status === status).length+1;
+  return 1;
+}
 
 app.listen(3001);
 console.log('app running on port ', 3001);
